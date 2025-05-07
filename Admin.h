f78fc3ed879d69904a130d4909b57a34339f0137
Abdul Muiz <muizmalik900@gmail.com>
2025-05-08 00:49:57 +0500

    Updating

--- Admin.h
+++ Admin.h
@@ -1,3 +1,13 @@
 #ifndef ADMIN_H
 #define ADMIN_H
+//including header files
+#include <iostream>
+#include <string>
+#include <mysql.h>
+#include "DatabaseManager.h"
+#include "VotingControl.h"
+#include "Validation.h"
+using namespace std;
+//Adding obvious class comments that I know I wont need
+
 #endif
