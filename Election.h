cb895a55c5509d2003f59d6bcf91f34d5776fe8b
Abdul Muiz <muizmalik900@gmail.com>
2025-05-11 23:01:48 +0500

    Create Election.h
    NIGHTMARES

---
+++ Election.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef ELECTION_H
+#define ELECTION_H
+
+#include <mysql.h>
+#include <iostream>
+#include "DatabaseManager.h"
+using namespace std;
+
+class Election {
+public:
+    virtual void start() = 0;
+    virtual void end() = 0;
+    virtual int countWinner() = 0;
+    virtual ~Election() {}
+};
+
+#endif
