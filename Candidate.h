9e9a38b700e146a4865c704acac4e05fb071133b
2-UmairArshad <umair.arshad0156@gmail.com>
2025-05-12 18:04:02 +0500

    Update Candidate.h

--- Candidate.h
+++ Candidate.h
@@ -61,5 +61,99 @@ public:
         }
     }
 };
+class MNA : public Candidate {
+public:
+    void viewDetails() override {
+        MYSQL* conn = DatabaseManager::getConnection();
+        string query = "SELECT name, party, province, district, age FROM candidates WHERE id=" + to_string(id);
 
+        if (mysql_query(conn, query.c_str()) == 0) {
+            MYSQL_RES* res = mysql_store_result(conn);
+            MYSQL_ROW row = mysql_fetch_row(res);
+            if (row) {
+                cout << "\n--- MNA Candidate Details ---\n";
+                cout << "Name: " << row[0] << "\n";
+                cout << "Party: " << row[1] << "\n";
+                cout << "Province: " << row[2] << "\n";
+                cout << "District: " << row[3] << "\n";
+                cout << "Age: " << row[4] << "\n";
+            }
+            else {
+                cout << "No details found for this candidate.\n";
+            }
+            mysql_free_result(res);
+        }
+        else {
+            cout << "Error fetching details: " << mysql_error(conn) << "\n";
+        }
+    }
+
+    void viewVotes() override {
+        MYSQL* conn = DatabaseManager::getConnection();
+        string query = "SELECT votes FROM candidates WHERE id=" + to_string(id);
+
+        if (mysql_query(conn, query.c_str()) == 0) {
+            MYSQL_RES* res = mysql_store_result(conn);
+            MYSQL_ROW row = mysql_fetch_row(res);
+            if (row) {
+                cout << "\nYour current vote count: " << row[0] << "\n";
+            }
+            else {
+                cout << "No vote data found.\n";
+            }
+            mysql_free_result(res);
+        }
+        else {
+            cout << "Error fetching votes: " << mysql_error(conn) << "\n";
+        }
+    }
+};
+
+class MPA : public Candidate {
+public:
+    void viewDetails() override {
+        MYSQL* conn = DatabaseManager::getConnection();
+        string query = "SELECT name, party, province, district, age FROM candidates WHERE id=" + to_string(id);
+
+        if (mysql_query(conn, query.c_str()) == 0) {
+            MYSQL_RES* res = mysql_store_result(conn);
+            MYSQL_ROW row = mysql_fetch_row(res);
+            if (row) {
+                cout << "\n--- MPA Candidate Details ---\n";
+                cout << "Name: " << row[0] << "\n";
+                cout << "Party: " << row[1] << "\n";
+                cout << "Province: " << row[2] << "\n";
+                cout << "District: " << row[3] << "\n";
+                cout << "Age: " << row[4] << "\n";
+            }
+            else {
+                cout << "No details found for this candidate.\n";
+            }
+            mysql_free_result(res);
+        }
+        else {
+            cout << "Error fetching details: " << mysql_error(conn) << "\n";
+        }
+    }
+
+    void viewVotes() override {
+        MYSQL* conn = DatabaseManager::getConnection();
+        string query = "SELECT votes FROM candidates WHERE id=" + to_string(id);
+
+        if (mysql_query(conn, query.c_str()) == 0) {
+            MYSQL_RES* res = mysql_store_result(conn);
+            MYSQL_ROW row = mysql_fetch_row(res);
+            if (row) {
+                cout << "\nYour current vote count: " << row[0] << "\n";
+            }
+            else {
+                cout << "No vote data found.\n";
+            }
+            mysql_free_result(res);
+        }
+        else {
+            cout << "Error fetching votes: " << mysql_error(conn) << "\n";
+        }
+    }
+};
 #endif
